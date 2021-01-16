e19c9bf8986ab079ebe94b87bd3220155d550b83
ergulemre <emreergul@std.sehir.edu.tr>
2021-01-16 22:15:25 +0300

    First DevOps Project Commit

---
+++ src/main/java/com/devops/entity/JsonSet.java
@@ -0,0 +1,34 @@
+package com.devops.entity;
+
+public class JsonSet {
+	
+	public String name;
+	public String description;
+	public int thrillFactor;
+	public int vomitFactor;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getThrillFactor() {
+		return thrillFactor;
+	}
+	public void setThrillFactor(int thrillFactor) {
+		this.thrillFactor = thrillFactor;
+	}
+	public int getVomitFactor() {
+		return vomitFactor;
+	}
+	public void setVomitFactor(int vomitFactor) {
+		this.vomitFactor = vomitFactor;
+	}	
+}
---
+++ src/main/java/com/devops/entity/ThemeParkRide.java
@@ -0,0 +1,81 @@
+package com.devops.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+@Entity
+public class ThemeParkRide implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	  
+	@NotEmpty
+	private String name;
+	  
+	@NotEmpty
+	private String description;
+	  
+	private int thrillFactor;
+	private int vomitFactor;
+	  
+  
+	public Long getId() {
+		return id;
+	}
+		
+	public void setId(Long id) {
+		this.id = id;
+	}
+		
+	public String getName() {
+		return name;
+	}
+		
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public int getThrillFactor() {
+		return thrillFactor;
+	}
+	
+	public void setThrillFactor(int thrillFactor) {
+		this.thrillFactor = thrillFactor;
+	}
+	
+	public int getVomitFactor() {
+		return vomitFactor;
+	}
+	
+	public void setVomitFactor(int vomitFactor) {
+		this.vomitFactor = vomitFactor;
+	}
+
+	public ThemeParkRide(String name, String description, int thrillFactor, int vomitFactor) {
+	    this.name = name;
+	    this.description = description;
+	    this.thrillFactor = thrillFactor;
+	    this.vomitFactor = vomitFactor;
+	}
+  
+  public ThemeParkRide() {
+	  super();
+  }
+}
